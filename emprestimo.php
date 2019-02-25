34c3dcb8ce618da1c721335af51d511328d2924e
iuryeng <38250160+iuryeng@users.noreply.github.com>
2019-02-25 15:09:08 -0300

    Criação da página emprestimos
    A página web permite que professores possam reservar materiais de multimídia

---
+++ emprestimo.php
@@ -0,0 +1,387 @@
+
+
+<?php include_once ('head.php') ?>
+<?php include_once ('header.php') ?>
+<?php include_once ('sidebar.php') ?>
+
+
+
+
+ <!-- DataTables -->
+ <link rel="stylesheet" href="plugins/datatables/dataTables.bootstrap.css">
+
+
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+<!-- Main content -->
+<section class="content-header">
+	<h1>
+	Reserva de Material
+		<small>Metodologia FICCG</small>
+	</h1>
+	<ol class="breadcrumb">
+	<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+	<li><a href="#">Gestão</a></li>	
+	</ol>
+
+       
+</section>
+
+<section class="content">
+
+
+                    <div class="box box-solid">
+                <div class="box-header with-border">
+                  <h3 class="box-title">Conduta Profissional de Reserva e Devolução</h3>
+                </div><!-- /.box-header -->
+                <div class="box-body">
+
+                  <div class="box-group" id="accordion">
+                    <!-- we are adding the .panel class so bootstrap.js collapse plugin detects it -->
+                    <div class="panel box box-primary">
+                      <div class="box-header with-border">
+                        <h4 class="box-title">
+                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" class="">
+                            Reserva e Emprestimo
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseOne" class="panel-collapse collapse in" aria-expanded="true" style="">
+                        <div class="box-body">
+                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="panel box box-danger">
+                      <div class="box-header with-border">
+                        <h4 class="box-title">
+                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" class="collapsed" aria-expanded="false">
+                            Uso do Material
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseTwo" class="panel-collapse collapse" aria-expanded="false" style="height: 0px;">
+                        <div class="box-body">
+                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                      </div>
+                    </div>
+                    <div class="panel box box-success">
+                      <div class="box-header with-border">
+                        <h4 class="box-title">
+                          <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree" class="collapsed" aria-expanded="false">
+                            Devolução 
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="collapseThree" class="panel-collapse collapse" aria-expanded="false">
+                        <div class="box-body">
+                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div><!-- /.box-body -->
+              </div>
+
+
+
+<div class="callout callout-info" style="margin-bottom: 0!important;">
+            <h4><i class="fa fa-info"></i> Nota:</h4>
+           Antes de reservar o material é importante observar a tabela abaixo. Veja se o horário de reserva que deseja está disponível.
+                    </div></br>
+
+
+<div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Tabela de Reserva de Material dos Professores FIC </h3>
+                  <div class="box-tools">
+                    <div class="input-group" style="width: 150px;">
+                    
+                    </div>
+                  </div>
+                </div><!-- /.box-header -->
+                <div class="box-body table-responsive no-padding">
+                  <table class="table table-hover">
+                    <tbody><tr>
+                      
+                      <th>Data e Hora</th>
+                      <th>Nome</th>
+                      <th>Curso</th>
+                      <th>Equipamento</th>
+                    </tr>
+
+                      <td>21/12/2017 18:30 - 21/12/2017 21:30</td>
+                      <td>Flaviane</td>
+                      <td>Organizador de Eventos</td>
+                      <td>DataShow</td>
+                    </tr>
+
+                    <tr>
+
+                      <td>21/12/2017 18:30 - 21/12/2017 21:30</td>
+                      <td>Ana Paula</td>
+                      <td>O.P Computador</td>
+                      <td>Laboratório de Informática e DataShow</td>
+                    </tr>
+
+                    <tr>
+                    
+                      <td>21/12/2017 18:30 - 21/12/2017 21:30</td>
+                      <td>Júlio Cesar</td>
+                      <td>O.P Telemarketing</td>
+                      <td>Sala de Multimídia</td>
+
+                    </tr>
+
+
+                    <tr>
+                    
+                      <td>21/12/2017 18:30 - 21/12/2017 21:30</td>
+                      <td>Michelinne</td>
+                      <td>Cuidador de Idosos</td>
+                      <td>Televisão</td>
+                      
+                    </tr>
+
+
+                    <tr>
+                    
+                      <td>21/12/2017 18:30 - 21/12/2017 21:30</td>
+                      <td>Marcílio</td>
+                      <td>Vendedor </td>
+                      <td>DataShow</td>
+                      
+                    </tr>
+
+
+
+
+
+
+
+
+
+
+                    
+                  </tbody></table>
+                </div><!-- /.box-body -->
+
+
+              </div>
+
+<section class="content timeline-body">
+<ul class="timeline">
+
+ <!-- timeline time label -->
+    <li class="time-label">
+        <span class="bg-yellow">
+           Data e Hora para Reserva
+        </span>
+    </li>
+    <!-- /.timeline-label -->
+
+    <!-- timeline item -->
+    <li>
+        <!-- timeline icon -->
+        <i class="fa fa-calendar bg-yellow"></i>
+        <div class="timeline-item">
+            <span class="time"><i class=""></i></span>
+
+           <h3 class="timeline-header"><i> Data e Hora </i></h3>
+
+            <div class="timeline-body">
+
+                <div class="box-header">
+                  <h3 class="box-title"></h3>
+                </div>
+                
+                  
+
+                  <!-- Date and time range -->
+                  <div class="form-group">
+                    <label>Data e hora para reserva</label>
+                    <div class="input-group">
+                      <div class="input-group-addon">
+                        <i class="fa fa-clock-o"></i>
+                      </div>
+                      <input type="text" class="form-control pull-right" id="reservationtime">
+                    </div><!-- /.input group -->
+                  </div><!-- /.form group -->
+
+              
+           
+           
+        </div>
+    </li>
+    <!-- END timeline item -->
+
+    <!-- timeline time label -->
+   
+    <!-- /.timeline-label -->
+
+    <!-- timeline item -->
+  
+    <!-- END timeline item -->
+
+    <!-- timeline time label -->
+    <li class="time-label">
+        <span class="bg-purple">
+           Equipamentos para Reserva
+        </span>
+    </li>
+    <!-- /.timeline-label -->
+
+    <!-- timeline item -->
+    <li>
+        <!-- timeline icon -->
+        <i class="fa  fa-plug bg-purple"></i>
+        <div class="timeline-item">
+            <span class="time"><i class=""></i></span>
+
+            <h3 class="timeline-header"><i> Equipamentos</i></h3>
+
+            <div class="timeline-body">
+
+
+                    <div class="form-group">
+                    <label>Equipamentos para reserva</label>
+                    <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" tabindex="-1" aria-hidden="true">
+                      
+                      <option>DataShow</option>
+                      <option>Televisão</option>
+                      <option>Sala de multimídia</option>
+                      <option>Laboratório de Informática</option>
+                      <option>Auditório</option>
+                    </select>
+                  </div>
+
+            </div>
+
+
+      
+        </div>
+    </li>
+
+      <!-- timeline item -->
+    <li>
+        <!-- timeline icon -->
+        <i class="fa fa-calendar-check-o bg-green"></i>
+        <div class="timeline-item">
+            <span class="time"><i class=""></i></span>
+
+            <h3 class="timeline-header"><i> Confirmação </i></h3>
+
+            <div class="timeline-body">
+<button class="btn btn-block btn-success btn-lg">Confirme aqui a reserva</button>
+           
+            </div>
+
+      
+        </div>
+    </li>
+
+  
+
+</ul>
+
+
+
+
+</section>
+
+</section>
+ </div> 
+<!-- jQuery 2.1.4 -->
+    <script src="plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <!-- Select2 -->
+    <script src="plugins/select2/select2.full.min.js"></script>
+    <!-- InputMask -->
+    <script src="plugins/input-mask/jquery.inputmask.js"></script>
+    <script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+    <script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
+    <!-- date-range-picker -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.2/moment.min.js"></script>
+    <script src="plugins/daterangepicker/daterangepicker.js"></script>
+    <!-- bootstrap color picker -->
+    <script src="plugins/colorpicker/bootstrap-colorpicker.min.js"></script>
+    <!-- bootstrap time picker -->
+    <script src="plugins/timepicker/bootstrap-timepicker.min.js"></script>
+    <!-- SlimScroll 1.3.0 -->
+    <script src="plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- iCheck 1.0.1 -->
+    <script src="plugins/iCheck/icheck.min.js"></script>
+    <!-- FastClick -->
+    <script src="plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="dist/js/app.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="dist/js/demo.js"></script>
+    <!-- Page script -->
+    <script>
+      $(function () {
+        //Initialize Select2 Elements
+        $(".select2").select2();
+
+        //Datemask dd/mm/yyyy
+       
+        //Datemask2 mm/dd/yyyy
+        $("#datemask2").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+        //Money Euro
+        $("[data-mask]").inputmask();
+
+        //Date range picker
+        $('#reservation').daterangepicker();
+        //Date range picker with time picker
+        $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'DD/MM/YYYY h:mm A'});
+        //Date range as a button
+        $('#daterange-btn').daterangepicker(
+            {
+              ranges: {
+                'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+              },
+              startDate: moment().subtract(29, 'days'),
+              endDate: moment()
+            },
+        function (start, end) {
+          $('#reportrange span').html(start.format('D MMM, YYYY') + ' - ' + end.format('D , YYYY'));
+        }
+        );
+
+        //iCheck for checkbox and radio inputs
+        $('input[type="checkbox"].minimal, input[type="radio"].minimal').iCheck({
+          checkboxClass: 'icheckbox_minimal-blue',
+          radioClass: 'iradio_minimal-blue'
+        });
+        //Red color scheme for iCheck
+        $('input[type="checkbox"].minimal-red, input[type="radio"].minimal-red').iCheck({
+          checkboxClass: 'icheckbox_minimal-red',
+          radioClass: 'iradio_minimal-red'
+        });
+        //Flat red color scheme for iCheck
+        $('input[type="checkbox"].flat-red, input[type="radio"].flat-red').iCheck({
+          checkboxClass: 'icheckbox_flat-green',
+          radioClass: 'iradio_flat-green'
+        });
+
+        //Colorpicker
+        $(".my-colorpicker1").colorpicker();
+        //color picker with addon
+        $(".my-colorpicker2").colorpicker();
+
+        //Timepicker
+        $(".timepicker").timepicker({
+          showInputs: false
+        });
+      });
+    </script>
+
+<?php include_once('footer.php');?>
